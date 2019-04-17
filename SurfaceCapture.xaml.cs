37e09e16584616534b1ca5bccdfe20e3b8daba18
seanD111 <sean_delong@hotmail.ca>
2019-04-17 17:45:06 -0400

    attempting to fix event bubbling errors

--- SurfaceCapture.xaml.cs
+++ SurfaceCapture.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -190,6 +191,7 @@ namespace TabletNotifier
 
         private bool IsTouch(TouchEventArgs e)
         {
+            Debug.WriteLine(e.GetType());
             if (e.TouchDevice != null)
             {
                 return true;
