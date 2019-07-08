97fc28b946f23ee74f1e75e577979247a6d83679
seanD111 <sean_delong@hotmail.ca>
2019-07-08 17:20:40 -0400

    removed OSC dependency

--- TabletStateClient.cs
+++ TabletStateClient.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using VVVV.Utils.OSC;
 using System.Diagnostics;
 
 /* Reference of the possible input components:
